c4aad12c94c9d9b7d71ffc0d9f105f835fbee93e
Markus Stawbom <ztawiz@gmail.com>
2020-11-07 21:06:44 +0100

    testemil

---
+++ testPryl.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+
+}
+
+void loop() {
+  for (int i = 6; i <= 9; i++){
+    digitalWrite(i, HIGH);
+    delay(1000);
+    digitalWrite(i, LOW);
+  }
+
+}
